29e6a2b581b3a18f4725c20f043b01e973dfb04c
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-26 18:28:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click',()=>{func()});
+
+function func(event) {
+	document.getElementById('status').innerHTML=`<h1>Entered Metaverse</h1>`;
+}
